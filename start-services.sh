5ce131b7a68ace7cdaa9d4306b6e128851b5c135
Kord Campbell <kordless@gmail.com>
2016-01-21 10:00:20 -0800

    templating

--- start-services.sh
+++ start-services.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# generate a random hash
+ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 5 | head -n 1)
+
+# build minecraft.yaml and minecraft-service.yaml
+sed -e "
+s,%ID%,$ID,g;
+" minecraft-template.yaml > minecraft-ser
+
+sed -e "
+s,%ID%,$ID,g;
+" minecraft-service-template.yaml > minecraft-service.yaml
+
+# start the services
 kubectl create -f minecraft.yaml
 kubectl create -f minecraft-service.yaml
+
+# cleanup
+rm minecraft.yaml minecraft-service.yaml
